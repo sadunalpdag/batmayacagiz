fe6f3ff2efb41870ae91379da501d68630185c25
rage <sadun.alpdag@gmail.com>
2022-02-22 00:51:31 +0300

    degsim vaar

--- supertrend_func.py
+++ supertrend_func.py
@@ -5,7 +5,7 @@ count_supertrend=0
 def supertrend(data4,data3,data,price_coin_now):
     global count_supertrend
     st = ta.supertrend(data4['high'], data3['low'], data['close'], length=10, multiplier=4.0, append=True)
-    print(st)
+    print(st.tail(10))
     ser_singleCol2 = st.iloc[:, 0]  # bununla data set içindeki 1. kolonu cekiyorum
     stlast= (ser_singleCol2.iloc[-1])  # bunu ile dataframedeki son syaıyı alıyorum
     print(stlast)
