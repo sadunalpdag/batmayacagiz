7145d521be0a1ad2600a66859e7e0e334f6a2b87
sadunalpdag <sadun.alpdag@gmail.com>
2022-05-22 14:31:11 +0300

    gonder gelsin

---
+++ xyz.py
@@ -0,0 +1,14 @@
+import schedule
+import time
+import tweepy
+
+from binance.client import Client
+
+
+import functions as func
+import signal_func as signals
+import time
+
+import key
+import send_msg as tele
+signals.sinyal2()
