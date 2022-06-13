4ea04ea473e90c7147e9804e1d574c57ea12f47a
sadunalpdag <sadun.alpdag@gmail.com>
2022-06-13 19:19:53 +0300

    gonder gelsin

--- classema_macd.py
+++ classema_macd.py
@@ -63,6 +63,8 @@ class Macdema():
 
 
 
+
+
     def dfall(self,symbol,timeframe):
 
 
@@ -76,14 +78,16 @@ class Macdema():
             'enableRateLimit': True,
             'adjustForTimeDifference': True
         })
+        order_approve = func.open_order_number(self.symbol)
 
         try:
-            if self.shortgiris==1 or self.longgiris==1:#alıs satıstan sonra 100 cycledan sonra tekrar işleme açma
+            if self.shortgiris==1 or self.longgiris==1 or order_approve == 0:#alıs satıstan sonra 100 cycledan sonra tekrar işleme açma
                 self.sayici_giris_control += 1
                 if self.sayici_giris_control ==15:
                     print(symbol,timeframe,self.sayici_giris_control)
                     self.longgiris=0
                     self.shortgiris=0
+                    self.sayici_giris_control=0
 
 
             else:
@@ -120,10 +124,10 @@ class Macdema():
 
                 if self.shortgiris!=1 and self.longgiris!=1:
                     if self.kesisimmacdsayac == 0:
-                        if (df["macd"][len(df.index) - 3] < df["slow macd"][len(df.index) - 3] and df["macd"][
-                            len(df.index) - 2] > df["slow macd"][len(df.index) - 2]) or (
-                                df["macd"][len(df.index) - 3] > df["slow macd"][len(df.index) - 3] and df["macd"][
-                            len(df.index) - 2] < df["slow macd"][len(df.index) - 2]):
+                        if (df["macd"][len(df.index) - 2] < df["slow macd"][len(df.index) - 2] and df["macd"][
+                            len(df.index) - 1] > df["slow macd"][len(df.index) - 1]) or (
+                                df["macd"][len(df.index) - 2] > df["slow macd"][len(df.index) - 2] and df["macd"][
+                            len(df.index) - 1] < df["slow macd"][len(df.index) - 1]):
                             self.kesisimmacdsayac += 1
                             macdkesisim = "macdkesisim"" "+ self.symbol+" "+ self.timeframe
                             tele.telegram_bot(macdkesisim)
@@ -134,10 +138,10 @@ class Macdema():
 
                     if self.kesisimmovaverage == 0:
 
-                        if (df["Fast Ema"][len(df.index) - 3] < df["Slow Ema"][len(df.index) - 3] and df["Fast Ema"][
-                            len(df.index) - 2] > df["Slow Ema"][len(df.index) - 2]) or (
-                                df["Fast Ema"][len(df.index) - 3] > df["Slow Ema"][len(df.index) - 3] and df["Fast Ema"][
-                            len(df.index) - 2] < df["Slow Ema"][len(df.index) - 2]):
+                        if (df["Fast Ema"][len(df.index) - 2] < df["Slow Ema"][len(df.index) - 2] and df["Fast Ema"][
+                            len(df.index) - 1] > df["Slow Ema"][len(df.index) - 1]) or (
+                                df["Fast Ema"][len(df.index) - 2] > df["Slow Ema"][len(df.index) - 2] and df["Fast Ema"][
+                            len(df.index) - 1] < df["Slow Ema"][len(df.index) - 1]):
                             self.kesisimmovaverage += 1
                             movkesisim = "movkesisim"+" "+ self.symbol+" "+self.timeframe
                             tele.telegram_bot(movkesisim)
@@ -217,8 +221,24 @@ coin15=Macdema('TRXUSDT',"1h",200,1.01,0.991)
 coin16=Macdema('LRCUSDT',"1h",50,1.01,0.991)
 coin17=Macdema('SANDUSDT',"1h",25,1.01,0.991)
 coin18=Macdema('AVAXUSDT',"1h",1,1.01,0.991)
-coin19=Macdema('ALGOUSDT',"1h",75,1.01,0.991)
-coin20=Macdema('ZECUSDT',"1h",0.3,1.01,0.991)
+coin19=Macdema('COTIUSDT',"1h",220,1.01,0.991)
+coin20=Macdema('LINKUSDT',"1h",4,1.01,0.991)
+coin21=Macdema('NEARUSDT',"1h",9,1.01,0.991)
+
+
+coin24=Macdema('MATICUSDT',"1h",40,1.01,0.991)
+coin25=Macdema('BELUSDT',"1h",30,1.01,0.991)
+coin26=Macdema('KNCUSDT',"1h",20,1.01,0.991)
+coin27=Macdema('BLZUSDT',"1h",200,1.01,0.991)
+coin28=Macdema('KAVAUSDT',"1h",15,1.01,0.991)
+coin29=Macdema('CHZUSDT',"1h",250,1.01,0.991)
+coin30=Macdema('DASHUSDT',"1h",1,1.01,0.991)
+coin31=Macdema('QTUMUSDT',"1h",8,1.01,0.991)
+coin32=Macdema('CHRUSDT',"1h",150,1.01,0.991)
+coin33=Macdema('OCEANUSDT',"1h",100,1.01,0.991)
+coin34=Macdema('ALPHAUSDT',"1h",150,1.01,0.991)
+
+
 
 coin2a=Macdema('ETHUSDT',"15m",0.015,1.007,0.993)
 coin1a=Macdema('BTCUSDT',"15m",0.001,1.007,0.993)
@@ -239,33 +259,26 @@ coin15a=Macdema('TRXUSDT',"15m",200,1.007,0.993)
 coin16a=Macdema('LRCUSDT',"15m",50,1.007,0.993)
 coin17a=Macdema('SANDUSDT',"15m",25,1.007,0.993)
 coin18a=Macdema('AVAXUSDT',"15m",1,1.007,0.993)
-coin19a=Macdema('ALGOUSDT',"15m",75,1.007,0.993)
-coin20a=Macdema('ZECUSDT',"15m",0.3,1.007,0.993)
 
-"""
+coin19a=Macdema('COTIUSDT',"15m",220,1.007,0.993)
+coin20a=Macdema('LINKUSDT',"15m",4,1.007,0.993)
+coin21a=Macdema('NEARUSDT',"15m",9,1.007,0.993)
 
-coin2b=Macdema('ETHUSDT',"4h",0.015,1.015,0.985)
-coin1b=Macdema('BTCUSDT',"4h",0.001,1.015,0.985)
-coin3b=Macdema('ATOMUSDT',"4h",2,1.015,0.985)
-coin4b=Macdema('EOSUSDT',"4h",20,1.015,0.985)
-coin5b=Macdema('LITUSDT',"4h",40,1.015,0.985)
-coin6b=Macdema('BNBUSDT',"4h",0.1,1.015,0.985)
 
-coin7b=Macdema('CRVUSDT',"4h",22,1.015,0.985)
-coin8b=Macdema('THETAUSDT',"4h",20,1.015,0.985)
-coin9b=Macdema('XRPUSDT',"4h",60,1.015,0.985)
-coin10b=Macdema('RUNEUSDT',"4h",10,1.015,0.985)
-coin11b=Macdema('ARUSDT',"4h",2,1.015,0.985)
-coin12b=Macdema('DOTUSDT',"4h",3,1.015,0.985)
-coin13b=Macdema('ETCUSDT',"4h",1.5,1.015,0.985)
-coin14b=Macdema('ALGOUSDT',"4h",80,1.015,0.985)
-coin15b=Macdema('TRXUSDT',"4h",200,1.015,0.985)
-coin16b=Macdema('LRCUSDT',"4h",50,1.015,0.985)
-coin17b=Macdema('SANDUSDT',"4h",25,1.015,0.985)
-coin18b=Macdema('AVAXUSDT',"4h",1,1.015,0.985)
-coin19b=Macdema('ALGOUSDT',"4h",75,1.015,0.985)
-coin20b=Macdema('ZECUSDT',"4h",0.3,1.015,0.985)
-"""
+coin24a=Macdema('MATICUSDT',"15m",40,1.007,0.993)
+coin25a=Macdema('BELUSDT',"15m",30,1.007,0.993)
+coin26a=Macdema('KNCUSDT',"15m",20,1.007,0.993)
+coin27a=Macdema('BLZUSDT',"15m",200,1.007,0.993)
+coin28a=Macdema('KAVAUSDT',"15m",15,1.007,0.993)
+coin29a=Macdema('CHZUSDT',"15m",250,1.007,0.993)
+coin30a=Macdema('DASHUSDT',"15m",1,1.007,0.993)
+coin31a=Macdema('QTUMUSDT',"15m",8,1.007,0.993)
+coin32a=Macdema('CHRUSDT',"15m",150,1.007,0.993)
+coin33a=Macdema('OCEANUSDT',"15m",100,1.007,0.993)
+coin34a=Macdema('ALPHAUSDT',"15m",150,1.007,0.993)
+
+
+
 while True:
 
     coin2.dfall('ETHUSDT',"1h")
@@ -304,10 +317,38 @@ while True:
     time.sleep(10)
     coin18.dfall('AVAXUSDT', "1h")
     time.sleep(10)
-    coin19.dfall('ALGOUSDT', "1h")
+    coin19.dfall('COTIUSDT', "1h")
     time.sleep(10)
-    coin20.dfall('ZECUSDT', "1h")
+    coin20.dfall('LINKUSDT', "1h")
     time.sleep(10)
+    coin21.dfall('NEARUSDT', "1h")
+    time.sleep(10)
+
+
+
+    coin24.dfall('MATICUSDT', "1h")
+    time.sleep(10)
+    coin25.dfall('BELUSDT', "1h")
+    time.sleep(10)
+    coin26.dfall('KNCUSDT', "1h")
+    time.sleep(10)
+    coin27.dfall('BLZUSDT', "1h")
+    time.sleep(10)
+    coin28.dfall('KAVAUSDT', "1h")
+    time.sleep(10)
+    coin29.dfall('CHZUSDT', "1h")
+    time.sleep(10)
+    coin30.dfall('DASHUSDT', "1h")
+    time.sleep(10)
+    coin31.dfall('QTUMUSDT', "1h")
+    time.sleep(10)
+    coin32.dfall('CHRUSDT', "1h")
+    time.sleep(10)
+    coin33.dfall('OCEANUSDT', "1h")
+    time.sleep(10)
+    coin34.dfall('ALPHAUSDT', "1h")
+    time.sleep(10)
+    
     coin2a.dfall('ETHUSDT', "15m")
     time.sleep(10)
     coin1a.dfall('BTCUSDT', "15m")
@@ -344,9 +385,36 @@ while True:
     time.sleep(10)
     coin18a.dfall('AVAXUSDT', "15m")
     time.sleep(10)
-    coin19a.dfall('ALGOUSDT', "15m")
+    coin19a.dfall('COTIUSDT', "15m")
     time.sleep(10)
-    coin20a.dfall('ZECUSDT', "15m")
+    coin20a.dfall('LINKUSDT', "15m")
+    time.sleep(10)
+    coin21a.dfall('NEARUSDT', "15m")
+    time.sleep(10)
+
+    coin24a.dfall('MATICUSDT', "15m")
+    time.sleep(10)
+    coin25a.dfall('BELUSDT', "15m")
+    time.sleep(10)
+    coin26a.dfall('KNCUSDT', "15m")
+    time.sleep(10)
+    coin27a.dfall('BLZUSDT', "15m")
+    time.sleep(10)
+    coin28a.dfall('KAVAUSDT', "15m")
+    time.sleep(10)
+    coin29a.dfall('CHZUSDT', "15m")
+    time.sleep(10)
+    coin30a.dfall('DASHUSDT', "15m")
+    time.sleep(10)
+    coin31a.dfall('QTUMUSDT', "15m")
+    time.sleep(10)
+    coin32a.dfall('CHRUSDT', "15m")
+    time.sleep(10)
+    coin33a.dfall('OCEANUSDT', "15m")
+    time.sleep(10)
+    coin34a.dfall('ALPHAUSDT', "15m")
+    time.sleep(10)
+
 
     coin2a.addspread(1, 1, 1, 2, 1, 3, 1, 4, 1, 5, 1, 6)
     time.sleep(10)
@@ -396,52 +464,39 @@ while True:
     time.sleep(10)
     coin19a.addspread(19, 1, 19, 2, 19, 3, 19, 4, 19, 5, 19, 6)
     time.sleep(10)
-    """
+    coin20a.addspread(20, 1, 20, 2, 20, 3, 20, 4, 20, 5, 20, 6)
     time.sleep(10)
-    coin2b.dfall('ETHUSDT', "4h")
+    coin21a.addspread(21, 1, 21, 2, 21, 3, 21, 4, 21, 5, 21, 6)
     time.sleep(10)
-    coin1b.dfall('BTCUSDT', "4h")
+
+    coin24a.addspread(24, 1, 24, 2, 24, 3, 24, 4, 24, 5, 24, 6)
     time.sleep(10)
-    coin3b.dfall('ATOMUSDT', "4h")
+    coin25a.addspread(25, 1, 25, 2, 25, 3, 25, 4, 25, 5, 25, 6)
     time.sleep(10)
-    coin4b.dfall('EOSUSDT', "4h")
+    coin26a.addspread(26, 1, 26, 2, 26, 3, 26, 4, 26, 5, 26, 6)
     time.sleep(10)
-    coin5b.dfall('LITUSDT', "4h")
+    coin27a.addspread(27, 1, 27, 2, 27, 3, 27, 4, 27, 5, 27, 6)
     time.sleep(10)
-    coin6b.dfall('BNBUSDT', "4h")
+    coin28a.addspread(28, 1, 28, 2, 28, 3, 28, 4, 28, 5, 28, 6)
     time.sleep(10)
-    coin7b.dfall('CRVUSDT', "4h")
+    coin29a.addspread(29, 1, 29, 2, 29, 3, 29, 4, 29, 5, 29, 6)
     time.sleep(10)
-    coin8b.dfall('THETAUSDT', "4h")
+    coin30a.addspread(30, 1, 30, 2, 30, 3, 30, 4, 30, 5, 30, 6)
     time.sleep(10)
-    coin9b.dfall('XRPUSDT', "4h")
+    coin31a.addspread(31, 1, 31, 2, 31, 3, 31, 4, 31, 5, 31, 6)
     time.sleep(10)
-    coin10b.dfall('RUNEUSDT', "4h")
+    coin32a.addspread(32, 1, 32, 2, 32, 3, 32, 4, 32, 5, 32, 6)
     time.sleep(10)
-    coin11b.dfall('ARUSDT', "4h")
+    coin33a.addspread(33, 1, 33, 2, 33, 3, 33, 4, 33, 5, 33, 6)
     time.sleep(10)
-    coin12b.dfall('DOTUSDT', "4h")
+    coin34a.addspread(34, 1, 34, 2, 34, 3, 34, 4, 34, 5, 34, 6)
     time.sleep(10)
-    coin13b.dfall('ETCUSDT', "4h")
-    time.sleep(10)
-    coin14b.dfall('ALGOUSDT', "4h")
-    time.sleep(10)
-    coin15b.dfall('TRXUSDT', "4h")
-    time.sleep(10)
-    coin16b.dfall('LRCUSDT', "4h")
-    time.sleep(10)
-    coin17b.dfall('SANDUSDT', "4h")
-    time.sleep(10)
-    coin18b.dfall('AVAXUSDT', "4h")
-    time.sleep(10)
-    coin19b.dfall('ALGOUSDT', "4h")
-    time.sleep(10)
-    coin20b.dfall('ZECUSDT', "4h")
-"""
+    tele.telegram_bot('server online2')
 
 
 
-    time.sleep(600)
+
+    time.sleep(300)
 
 
 
