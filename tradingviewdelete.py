591bdd1b7136399d3552882920ead9572cd6dea8
sadunalpdag <sadun.alpdag@gmail.com>
2022-10-28 01:25:25 +0300

    tradingview

---
+++ tradingviewdelete.py
@@ -0,0 +1,332 @@
+import pandas as pd
+import ccxt, config
+
+import time
+
+import firebase_admin
+from firebase_admin import credentials, firestore
+from tradingview_ta import TA_Handler, Interval, Exchange
+import calendar
+
+
+longgiris = 0
+shortgiris = 0
+sayici_giris_control = 0
+sheetsymbolx = 0
+sheetssymboly = 0
+macdlast = 0
+engulfing = 0
+buysignallast = 0
+sellsignallast = 0
+coipricefloat = 0
+sellsignallast1 = 0
+sellsignallast2 = 0
+str_sellsignallast1 = 0
+str_sellsignallast2 = 0
+kimlik = credentials.Certificate("ema_class.json")
+
+app = firebase_admin.initialize_app(kimlik)
+
+
+class Macdema():
+
+    def __init__(self, symbol, timeframe, quantity, buyvalue, sellvalue):
+        self.symbol = symbol
+        self.longgiris = longgiris
+        self.shortgiris = shortgiris
+        self.timeframe = timeframe
+        self.sayici_giris_control = sayici_giris_control
+        self.quantity = quantity
+        self.buyvalue = buyvalue
+        self.sellvalue = sellvalue
+        self.buysignallast = buysignallast
+        self.sellsignallast = sellsignallast
+        self.sellsignallast1 = sellsignallast1
+        self.sellsignallast2 = sellsignallast2
+        self.str_sellsignallast1 = str_sellsignallast1
+        self.str_sellsignallast2 = str_sellsignallast2
+
+        self.coipricefloat = coipricefloat
+
+    def dfall(self, symbol, timeframe):
+
+
+        try:
+            handler = TA_Handler(
+                symbol=symbol,
+                exchange="BINANCE",
+                screener="crypto",
+                interval=timeframe,
+                timeout=None,
+                proxies={'http': '47.242.84.173:3128', 'http': '181.205.20.195:999', 'http': '192.111.135.17:18302',
+                         'http': '103.108.228.185:7497'}
+            )
+
+            analysis = handler.get_analysis().summary
+
+            analysis_str = str(analysis)
+
+            symbolrec = analysis['RECOMMENDATION']
+            dt = time.gmtime()
+            ts = calendar.timegm(dt)
+            ts_str = str(ts)
+
+            x = self.symbol + self.timeframe + ts_str
+
+            db = firestore.client()  # db e baglantı
+
+            docs = db.collection (self.symbol + self.timeframe).get()
+
+            for doc in docs:
+                key = doc.id
+                db.collection(self.symbol + self.timeframe).document(key).delete()
+                print("silindi" + self.symbol + self.timeframe)
+
+            document = db.collection(self.symbol + self.timeframe).document(x)
+            docId = document.id
+            document.set({
+                "id": ts_str,
+
+                "position": symbolrec,
+
+            })
+
+
+
+        except :
+            print("silindi2")
+
+
+coin2 = Macdema('API3USDT', "1h", 15, 1.007, 0.993)
+coin1 = Macdema('WOOUSDT', "1h", 100, 1.007, 0.993)
+coin3 = Macdema('CELOUSDT', "1h", 30, 1.007, 0.993)
+coin4 = Macdema('ARPAUSDT', "1h", 600, 1.007, 0.993)
+coin5 = Macdema('LPTUSDT', "1h", 3, 1.007, 0.993)
+coin6 = Macdema('KLAYUSDT', "1h", 3, 1.007, 0.993)
+
+coin7 = Macdema('OMGUSDT', "1h", 15, 1.007, 0.993)
+coin8 = Macdema('OPUSDT', "1h", 40, 1.007, 0.993)
+coin9 = Macdema('UNFIUSDT', "1h", 6, 1.007, 0.993)
+
+coin11 = Macdema('ARUSDT', "1h", 2, 1.007, 0.993)
+coin12 = Macdema('DOTUSDT', "1h", 3, 1.007, 0.993)
+coin13 = Macdema('ETCUSDT', "1h", 1.5, 1.007, 0.993)
+coin14 = Macdema('ALGOUSDT', "1h", 80, 1.007, 0.993)
+coin15 = Macdema('TRXUSDT', "1h", 200, 1.007, 0.993)
+coin16 = Macdema('LRCUSDT', "1h", 50, 1.007, 0.993)
+coin17 = Macdema('SANDUSDT', "1h", 25, 1.007, 0.993)
+
+coin18 = Macdema('YFIUSDT', "1h", 0.005, 1.007, 0.993)
+coin19 = Macdema('MASKUSDT', "1h", 3, 1.007, 0.993)
+coin20 = Macdema('SUSHIUSDT', "1h", 30, 1.007, 0.993)
+
+coin21 = Macdema('NEARUSDT', "1h", 9, 1.007, 0.993)
+coin24 = Macdema('MATICUSDT', "1h", 40, 1.007, 0.993)
+coin25 = Macdema('BELUSDT', "1h", 30, 1.007, 0.993)
+coin26 = Macdema('DOGEUSDT', "1h", 20, 1.007, 0.993)
+coin27 = Macdema('BLZUSDT', "1h", 200, 1.007, 0.993)
+coin28 = Macdema('KAVAUSDT', "1h", 15, 1.007, 0.993)
+coin29 = Macdema('CHZUSDT', "1h", 250, 1.007, 0.993)
+
+coin31 = Macdema('QTUMUSDT', "1h", 8, 1.007, 0.993)
+coin32 = Macdema('CHRUSDT', "1h", 150, 1.007, 0.993)
+coin33 = Macdema('OCEANUSDT', "1h", 100, 1.007, 0.993)
+coin34 = Macdema('ALPHAUSDT', "1h", 150, 1.007, 0.993)
+coin35 = Macdema('XRPUSDT', "1h", 100, 1.006, 0.994)
+coin36 =  Macdema('BNBUSDT', "1h", 0.13, 1.006, 0.994)
+coin37 = Macdema('ATOMUSDT', "1h", 4, 1.006, 0.994)
+
+
+
+
+coin2a = Macdema('API3USDT', "2h", 20, 1.007, 0.993)
+coin1a = Macdema('WOOUSDT', "2h", 125, 1.007, 0.993)
+coin3a = Macdema('CELOUSDT', "2h", 40, 1.007, 0.993)
+coin4a = Macdema('ARPAUSDT', "2h", 700, 1.007, 0.993)
+coin5a = Macdema('LPTUSDT', "2h", 4, 1.007, 0.993)
+coin6a = Macdema('KLAYUSDT', "2h", 3, 1.007, 0.993)
+
+coin7a = Macdema('OMGUSDT', "2h", 20, 1.007, 0.993)
+coin8a = Macdema('OPUSDT', "2h", 55, 1.007, 0.993)
+coin9a = Macdema('UNFIUSDT', "2h", 8, 1.007, 0.993)
+
+coin11a = Macdema('ARUSDT', "2h", 3, 1.007, 0.993)
+coin12a = Macdema('DOTUSDT', "2h", 4, 1.007, 0.993)
+coin13a = Macdema('ETCUSDT', "2h", 1.8, 1.007, 0.993)
+coin14a = Macdema('ALGOUSDT', "2h", 100, 1.007, 0.993)
+coin15a = Macdema('TRXUSDT', "2h", 240, 1.007, 0.993)
+coin16a = Macdema('LRCUSDT', "2h", 70, 1.007, 0.993)
+coin17a = Macdema('SANDUSDT', "2h", 30, 1.007, 0.993)
+
+coin18a = Macdema('YFIUSDT', "2h", 0.005, 1.007, 0.993)
+coin19a = Macdema('MASKUSDT', "2h", 3, 1.007, 0.993)
+coin20a = Macdema('SUSHIUSDT', "2h", 30, 1.007, 0.993)
+coin21a = Macdema('NEARUSDT', "2h", 12, 1.007, 0.993)
+coin24a = Macdema('MATICUSDT', "2h", 45, 1.007, 0.993)
+coin25a = Macdema('BELUSDT', "2h", 55, 1.007, 0.993)
+coin26a = Macdema('DOGEUSDT', "2h", 66,1.007, 0.993)
+coin27a = Macdema('BLZUSDT', "2h", 77, 1.007, 0.993)
+coin28a = Macdema('KAVAUSDT', "2h", 20, 1.007, 0.993)
+coin29a = Macdema('CHZUSDT', "2h", 280, 1.007, 0.993)
+
+coin31a = Macdema('QTUMUSDT', "2h", 10, 1.007, 0.993)
+coin32a = Macdema('CHRUSDT', "2h", 180, 1.007, 0.993)
+coin33a = Macdema('OCEANUSDT', "2h", 90, 1.007, 0.993)
+coin34a = Macdema('ALPHAUSDT', "2h", 170, 1.007, 0.993)
+coin35a = Macdema('XRPUSDT', "2h", 100, 1.006, 0.994)
+
+coin36a = Macdema('BNBUSDT', "2h", 0.13, 1.006, 0.994)
+coin37a = Macdema('ATOMUSDT', "2h", 4, 1.006, 0.994)
+
+
+
+while True:
+    coin2.dfall('API3USDT', "1h")
+    time.sleep(2)
+    coin1.dfall('WOOUSDT', "1h")
+    time.sleep(2)
+    coin3.dfall('CELOUSDT', "1h")
+    time.sleep(2)
+    coin4.dfall('ARPAUSDT', "1h")
+    time.sleep(2)
+    coin5.dfall('LPTUSDT', "1h")
+    time.sleep(2)
+    coin6.dfall('KLAYUSDT', "1h")
+    time.sleep(2)
+
+    coin7.dfall('OMGUSDT', "1h")
+    time.sleep(2)
+    coin8.dfall('OPUSDT', "1h")
+    time.sleep(2)
+    coin9.dfall('UNFIUSDT', "1h")
+    time.sleep(2)
+    coin11.dfall('ARUSDT', "1h")
+    time.sleep(2)
+    coin12.dfall('DOTUSDT', "1h")
+    time.sleep(2)
+    coin13.dfall('ETCUSDT', "1h")
+    time.sleep(2)
+    coin14.dfall('ALGOUSDT', "1h")
+    time.sleep(2)
+    coin15.dfall('TRXUSDT', "1h")
+    time.sleep(2)
+    coin16.dfall('LRCUSDT', "1h")
+    time.sleep(2)
+    coin17.dfall('SANDUSDT', "1h")
+    time.sleep(2)
+
+    coin18.dfall('YFIUSDT', "1h")
+    time.sleep(30)
+    coin19.dfall('MASKUSDT', "1h")
+    time.sleep(30)
+
+    coin20.dfall('SUSHIUSDT', "1h")
+    time.sleep(30)
+
+    coin21.dfall('NEARUSDT', "1h")
+    time.sleep(2)
+
+    coin24.dfall('MATICUSDT', "1h")
+    time.sleep(2)
+    coin25.dfall('BELUSDT', "1h")
+    time.sleep(2)
+    coin26.dfall('DOGEUSDT', "1h")
+    time.sleep(2)
+    coin27.dfall('BLZUSDT', "1h")
+    time.sleep(2)
+    coin28.dfall('KAVAUSDT', "1h")
+    time.sleep(2)
+    coin29.dfall('CHZUSDT', "1h")
+    time.sleep(2)
+
+    coin31.dfall('QTUMUSDT', "1h")
+    time.sleep(2)
+    coin32.dfall('CHRUSDT', "1h")
+    time.sleep(2)
+    coin33.dfall('OCEANUSDT', "1h")
+    time.sleep(2)
+    coin34.dfall('ALPHAUSDT', "1h")
+    time.sleep(2)
+    coin35.dfall('XRPUSDT', "1h")
+    time.sleep(2)
+    coin36.dfall('BNBUSDT', "1h")
+    time.sleep(2)
+    coin37.dfall('ATOMUSDT', "1h")
+    time.sleep(2)
+
+
+
+
+    coin2a.dfall('API3USDT', "2h")
+    time.sleep(2)
+    coin1a.dfall('WOOUSDT', "2h")
+    time.sleep(2)
+    coin3a.dfall('CELOUSDT', "2h")
+    time.sleep(2)
+    coin4a.dfall('ARPAUSDT', "2h")
+    time.sleep(2)
+    coin5a.dfall('LPTUSDT', "2h")
+    time.sleep(2)
+    coin6a.dfall('KLAYUSDT', "2h")
+    time.sleep(2)
+
+    coin7a.dfall('OMGUSDT', "2h")
+    time.sleep(2)
+    coin8a.dfall('OPUSDT', "2h")
+    time.sleep(2)
+    coin9a.dfall('UNFIUSDT', "2h")
+    time.sleep(2)
+    coin11a.dfall('ARUSDT', "2h")
+    time.sleep(2)
+    coin12a.dfall('DOTUSDT', "2h")
+    time.sleep(2)
+    coin13a.dfall('ETCUSDT', "2h")
+    time.sleep(2)
+    coin14a.dfall('ALGOUSDT', "2h")
+    time.sleep(2)
+    coin15a.dfall('TRXUSDT', "2h")
+    time.sleep(2)
+    coin16a.dfall('LRCUSDT', "2h")
+    time.sleep(2)
+    coin17a.dfall('SANDUSDT', "2h")
+    time.sleep(2)
+
+    coin18a.dfall('YFIUSDT', "2h")
+    time.sleep(30)
+    coin19a.dfall('MASKUSDT', "2h")
+    time.sleep(30)
+
+    coin20a.dfall('SUSHIUSDT', "2h")
+    time.sleep(30)
+    coin21a.dfall('NEARUSDT', "2h")
+    time.sleep(2)
+
+    coin24a.dfall('MATICUSDT', "2h")
+    time.sleep(2)
+    coin25a.dfall('BELUSDT', "2h")
+    time.sleep(2)
+    coin26a.dfall('DOGEUSDT', "2h")
+    time.sleep(2)
+    coin27a.dfall('BLZUSDT', "2h")
+    time.sleep(2)
+    coin28a.dfall('KAVAUSDT', "2h")
+    time.sleep(2)
+    coin29a.dfall('CHZUSDT', "2h")
+    time.sleep(2)
+
+    coin31a.dfall('QTUMUSDT', "2h")
+    time.sleep(2)
+    coin32a.dfall('CHRUSDT', "2h")
+    time.sleep(2)
+    coin33a.dfall('OCEANUSDT', "2h")
+    time.sleep(2)
+    coin34a.dfall('ALPHAUSDT', "2h")
+    time.sleep(2)
+    coin35a.dfall('XRPUSDT', "2h")
+    time.sleep(2)
+    coin36a.dfall('BNBUSDT', "2h")
+    time.sleep(2)
+    coin37a.dfall('ATOMUSDT', "2h")
+    time.sleep(2)
+
+    time.sleep(3600)
